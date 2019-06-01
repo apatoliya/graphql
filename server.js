1277cbb8ab5d79cbd5dc1a17f75378138fa1c17b
apatoliya <apatoliya@gmail.com>
2019-06-01 15:47:13 -0500

    added react client, cors, apollo

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 let port = process.env.port || 5000;
 const schema = require('./schema');
+const cors = require('cors');
+
 
 
 
 let app = express();
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
       schema: schema,
